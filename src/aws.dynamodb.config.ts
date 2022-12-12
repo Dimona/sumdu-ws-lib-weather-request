c7d0547c2eb3c82742dbcc2ba806a40609ed0b2a
Dmytro Ieremenko <dieremenko@mindk.com>
2022-12-12 12:12:22 +0200

    fix: config with mocked dynamodb client

--- src/aws.dynamodb.config.ts
+++ src/aws.dynamodb.config.ts
@@ -2,7 +2,7 @@ import { registerAs } from '@nestjs/config';
 import { INDEX_TYPE, Table } from '@typedorm/common';
 import { AWS_DYNAMODB_API_VERSION, AwsDynamodbModuleOptions } from '@workshop/lib-nest-aws/dist/services/dynamodb';
 import { AWS_DYNAMODB_CONFIG } from './aws.dynamodb.constants';
-import { statusNextTimeIndex, WEATHER_REQUESTS } from './constants';
+import { statusNextTimeIndex, TEST_WEATHER_REQUESTS_TABLE, WEATHER_REQUESTS } from './constants';
 import { WeatherRequestEntity } from './entity';
 
 export const awsDynamodbConfig = registerAs(AWS_DYNAMODB_CONFIG, () => {
@@ -11,7 +11,7 @@ export const awsDynamodbConfig = registerAs(AWS_DYNAMODB_CONFIG, () => {
   return <AwsDynamodbModuleOptions>{
     client: {
       apiVersion: process.env.AWS_DYNAMODB_API_VERSION || AWS_DYNAMODB_API_VERSION,
-      endpoint: env === 'local' ? process.env.DYNAMODB_LOCAL : undefined,
+      endpoint: process.env.MOCK_DYNAMODB_ENDPOINT || (env === 'local' ? process.env.DYNAMODB_LOCAL : undefined),
       retryMode: 'standard',
     },
     connections: {
@@ -19,7 +19,7 @@ export const awsDynamodbConfig = registerAs(AWS_DYNAMODB_CONFIG, () => {
         table: new Table({
           name: `${
             process.env.MOCK_DYNAMODB_ENDPOINT
-              ? 'ws-weather-weather-requests-test'
+              ? TEST_WEATHER_REQUESTS_TABLE
               : process.env.WS_WEATHER_REQUESTS_DYNAMODB_TABLE
           }`,
           partitionKey: 'id',
--- src/constants.ts
+++ src/constants.ts
@@ -3,3 +3,5 @@ export const WEATHER_REQUESTS = 'WEATHER_REQUESTS';
 export const statusNextTimeIndex = 'status-nextTime-index';
 
 export const WEATHER_DATE_DEFAULT_FORMAT = 'DD.MM.YYYY';
+
+export const TEST_WEATHER_REQUESTS_TABLE = 'ws-weather-weather-requests-test';
